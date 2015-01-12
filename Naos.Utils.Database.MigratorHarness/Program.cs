4f6c401a335106e280120f0f3f56790871593141
Lawson Caudill <lawson@caudillclan.us>
2015-01-12 15:21:04 -0500

    Add a wrapper for FluentMigrator as well as a command line harness app to call it.

---
+++ Naos.Utils.Database.MigratorHarness/Program.cs
@@ -0,0 +1,110 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Naos">
+//   Copyright 2014 Naos
+// </copyright>
+// <summary>
+//   The program entry point.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Utils.Database.MigratorHarness
+{
+    using System;
+
+    using CLAP;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The program entry point.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            WriteAsciiArt();
+            Parser.Run<MigratorConsoleHarness>(args);
+        }
+
+        /// <summary>
+        /// Write ASCII art.
+        /// </summary>
+        private static void WriteAsciiArt()
+        {
+            Console.WriteLine(@"______________________________________________________________________________");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|                                   `.,,.`                                   |");
+            Console.WriteLine(@"|                                :++++++++++:                                |");
+            Console.WriteLine(@"|                             `'+++++++++++++#+.                             |");
+            Console.WriteLine(@"|                            '+++++++++++++++++++`                           |");
+            Console.WriteLine(@"|                          .++++++++++++++++++++++,                          |");
+            Console.WriteLine(@"|                         :+++++++++';::;'+++++++++;                         |");
+            Console.WriteLine(@"|                        ;+++++++;.        `;++#++++'                        |");
+            Console.WriteLine(@"|                       :++++++;`             :++++++;  `'                   |");
+            Console.WriteLine(@"|                      ,++++++.                `'+++++::++                   |");
+            Console.WriteLine(@"|                      ++++++`                   '++++++++                   |");
+            Console.WriteLine(@"|                     '+++++                      '+++++++`                  |");
+            Console.WriteLine(@"|                     +++++`                      `+++++++`                  |");
+            Console.WriteLine(@"|                    '++++:                      `++++++++`                  |");
+            Console.WriteLine(@"|                    +++++`                     :+++++++++`                  |");
+            Console.WriteLine(@"|                    ++++:                      `;++++++++.                  |");
+            Console.WriteLine(@"|                    ++++`                         ,'+++++.                  |");
+            Console.WriteLine(@"|                     :;.                            `:+++.                  |");
+            Console.WriteLine(@"|                                                       .;,                  |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|               ```````                              ```..`.```              |");
+            Console.WriteLine(@"|         `.,,,,,,,,,,,,,,,.`                    .;'''''''''''''';.`         |");
+            Console.WriteLine(@"|       `,,,,,,,,,,,,,,,,,,,,,`               `;'''''''''''''''''''';`       |");
+            Console.WriteLine(@"|      .,,,,,,,,,,,,,,,,,,,,,,,`              ''''''''''''''''''''''''       |");
+            Console.WriteLine(@"|      `,,,,,,,,,,,,,,,,,,,,,,,`             `.'''''''''''''''''''''',`      |");
+            Console.WriteLine(@"|      ,.`.,,,,,,,,,,,,,,,,,.`.,             .',.:;'''''''''''''';;,,'`      |");
+            Console.WriteLine(@"|      ,,,,..```.......``...,,,,             .''';:,,,,,,,:,,,,,,:;'''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      ,,,,,,,,,,,,,,,,,,,,,,,,,             .''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|      .,,,,,,,,,,,,,,,,,,,,,,,.             `''''''''''''''''''''''''`      |");
+            Console.WriteLine(@"|       .,,,,,,,,,,,,,,,,,,,,,.               .;'''''''''''''''''''';.       |");
+            Console.WriteLine(@"|         `.,,,,,,,,,,,,,,,.`                   `,:'''''''''''''':,`         |");
+            Console.WriteLine(@"|                                                      ``````                |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|                   ````````````````````````````````                         |");
+            Console.WriteLine(@"|          `````````````````````````````````````````````````````             |");
+            Console.WriteLine(@"|      ````````````````````.....................```````````````````          |");
+            Console.WriteLine(@"|     `````````````````.............,,,..............`````````````````       |");
+            Console.WriteLine(@"|      ```````````````````.......................````````````````````        |");
+            Console.WriteLine(@"|         ```````````````````````````````````````````````````````            |");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|----------------------------------------------------------------------------|");
+            Console.WriteLine(@"|                                                                            |");
+            Console.WriteLine(@"|         _____  ____    __  __ _                 _                          |");
+            Console.WriteLine(@"|        |  __ \|  _ \  |  \/  (_)               | |                         |");
+            Console.WriteLine(@"|        | |  | | |_) | | \  / |_  __ _ _ __ __ _| |_ ___  _ __              |");
+            Console.WriteLine(@"|        | |  | |  _ <  | |\/| | |/ _` | '__/ _` | __/ _ \| '__|             |");
+            Console.WriteLine(@"|        | |__| | |_) | | |  | | | (_| | | | (_| | || (_) | |                |");
+            Console.WriteLine(@"|        |_____/|____/  |_|  |_|_|\__, |_|  \__,_|\__\___/|_|                |");
+            Console.WriteLine(@"|                                  __/ |                                     |");
+            Console.WriteLine(@"|                                 |___/                                      |");
+            Console.WriteLine(@"|____________________________________________________________________________|");
+            Console.WriteLine();
+        }
+    }
+}

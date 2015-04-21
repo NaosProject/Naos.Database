2df72b11f4891792cbe7786a52d98cf0a8b93b71
Suraj Gupta <surajg@gmail.com>
2015-04-21 13:15:55 -0400

    adding models for Backup

---
+++ Naos.Database.Tools/Backup/BackupDetailsValidation.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BackupDetailsValidation.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.Tools.Backup
+{
+    /// <summary>
+    /// Validates a <see cref="BackupDetails"/>
+    /// </summary>
+    public static class BackupDetailsValidation
+    {
+        /// <summary>
+        /// Throws an exception if the <see cref="BackupDetails"/> is invalid.
+        /// </summary>
+        /// <param name="backupDetails">The backup details to validate.</param>
+        public static void ThrowIfInvalid(this BackupDetails backupDetails)
+        {
+
+        }
+
+    }
+}
---
+++ Naos.Database.Tools/Backup/Encryptor.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Encryptor.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.Tools.Backup
+{
+    /// <summary>
+    /// Specifies the encryptor to use when encrypting backups.
+    /// </summary>
+    public enum Encryptor
+    {
+        /// <summary>
+        /// Backup using a server certificate.
+        /// </summary>
+        ServerCertificate,
+
+        /// <summary>
+        /// Backup using an asymmetric key.
+        /// </summary>
+        ServerAsymmetricKey
+    }
+}

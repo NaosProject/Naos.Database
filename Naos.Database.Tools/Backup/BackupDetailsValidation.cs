7e5857cb4736dbc32c1bd81f568062f5c5b43500
Suraj Gupta <surajg@gmail.com>
2015-04-22 09:04:39 -0400

    added a validator for BackupDetails

--- Naos.Database.Tools/Backup/BackupDetailsValidation.cs
+++ Naos.Database.Tools/Backup/BackupDetailsValidation.cs
@@ -6,6 +6,10 @@
 
 namespace Naos.Database.Tools.Backup
 {
+    using System;
+
+    using CuttingEdge.Conditions;
+
     /// <summary>
     /// Validates a <see cref="BackupDetails"/>
     /// </summary>
@@ -17,8 +21,44 @@ namespace Naos.Database.Tools.Backup
         /// <param name="backupDetails">The backup details to validate.</param>
         public static void ThrowIfInvalid(this BackupDetails backupDetails)
         {
+            Condition.Requires(backupDetails.BackupTo, "backupDetails.BackupTo").IsNotNull();
 
+            if (!string.IsNullOrWhiteSpace(backupDetails.Name))
+            {
+                if (backupDetails.Name.Length > 128)
+                {
+                    throw new ArgumentException("Name cannot be more than 128 characters in length.");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(backupDetails.Description))
+            {
+                if (backupDetails.Description.Length > 255)
+                {
+                    throw new ArgumentException("Description cannot be more than 255 characters in length.");
+                }
+            }
+
+            if (backupDetails.Cipher != Cipher.NoEncryption)
+            {
+                if (backupDetails.Encryptor == Encryptor.None)
+                {
+                    throw new ArgumentException("Encryptor is required when any Cipher != NoEncryption");
+                }
+
+                if (string.IsNullOrWhiteSpace(backupDetails.EncryptorName))
+                {
+                    throw new ArgumentException("EncryptorName is required when any Cipher != NoEncryption.");
+                }
+            }
+
+            if (backupDetails.ChecksumOption == ChecksumOption.Checksum)
+            {
+                if (backupDetails.ErrorHandling == ErrorHandling.None)
+                {
+                    throw new ArgumentException("ErrorHandling cannot be None when using checksum.");
+                }
+            }
         }
-
     }
 }

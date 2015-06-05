84d43a1d63aa716f9b35dc25a0127fe9e326e928
Lawson Caudill <lawson@caudillclan.us>
2015-06-04 20:34:43 -0400

    Added missing stylecop file to a few projects, added messagebus handler (with contract) to backup databases on a local machine.

---
+++ Naos.Database.MessageBus.Contract/MessageHandlerSettings.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MessageHandlerSettings.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.MessageBus.Contract
+{
+    using System;
+
+    /// <summary>
+    /// Model object for Its.Configuration providing settings for the MessageHandlers.
+    /// </summary>
+    public class MessageHandlerSettings
+    {
+        /// <summary>
+        /// Gets or sets the connection string to use for local host database operations.
+        /// </summary>
+        public string LocalhostConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default timeout for use on database operations.
+        /// </summary>
+        public TimeSpan DefaultTimeout { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location on disk to stage backups.
+        /// </summary>
+        public string BackupDirectory { get; set; }
+    }
+}
---
+++ Naos.Database.MessageBus.Handlers/BackupMessageHandler.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BackupMessageHandler.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.MessageBus.Handlers
+{
+    using System;
+
+    using Its.Configuration;
+
+    using Naos.Database.MessageBus.Contract;
+    using Naos.Database.Tools;
+    using Naos.Database.Tools.Backup;
+    using Naos.MessageBus.HandlingContract;
+
+    /// <summary>
+    /// Naos.MessageBus handler for BackupMessages.
+    /// </summary>
+    public class BackupMessageHandler : IHandleMessages<BackupDatabaseMessage>
+    {
+        /// <inheritdoc />
+        public void Handle(BackupDatabaseMessage message)
+        {
+            Action<string> logAction = s => { };
+
+            var settings = Settings.Get<MessageHandlerSettings>();
+            var backupDetails = new BackupDetails()
+                                    {
+                                        Name = message.BackupName,
+                                        BackupTo = new Uri(settings.BackupDirectory),
+                                        ChecksumOption = ChecksumOption.Checksum,
+                                        Cipher = Cipher.NoEncryption,
+                                        CompressionOption = CompressionOption.NoCompression,
+                                        Description = message.Description,
+                                        Device = Device.Disk,
+                                        ErrorHandling = ErrorHandling.StopOnError,
+                                    };
+
+            DatabaseManager.BackupFull(
+                settings.LocalhostConnectionString,
+                message.DatabaseName,
+                backupDetails,
+                settings.DefaultTimeout,
+                logAction);
+        }
+    }
+}

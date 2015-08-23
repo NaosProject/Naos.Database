da8de0b253e4141443799f976b396c4754205137
Lawson Caudill <lawson@caudillclan.us>
2015-08-23 17:43:22 -0400

    Added restore database message and handler. Update packages.

---
+++ Naos.Database.MessageBus.Contract/RestoreDatabaseMessage.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RestoreDatabaseMessage.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.MessageBus.Contract
+{
+    using Naos.FileJanitor.MessageBus.Contract;
+    using Naos.MessageBus.DataContract;
+
+    /// <summary>
+    /// Message to initiate a database restore on the server the handler is on.
+    /// </summary>
+    public class RestoreDatabaseMessage : IMessage, IShareFilePath
+    {
+        /// <inheritdoc />
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the database to restore to.
+        /// </summary>
+        public string DatabaseName { get; set; }
+
+        /// <inheritdoc />
+        public string FilePath { get; set; }
+    }
+}

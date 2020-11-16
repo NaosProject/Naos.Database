6ee07945b420bd512a62588ecdc162627f529c7f
Lawson Caudill <lawson@getthinktank.com>
2020-11-16 10:36:07 +0000

    Couple of more model construction tests finished.

--- Naos.Database.Domain.Test/StreamRecordMetadataTest.cs
+++ Naos.Database.Domain.Test/StreamRecordMetadataTest.cs
@@ -7,14 +7,18 @@
 namespace Naos.Database.Domain.Test
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
-
+    using System.Linq;
     using FakeItEasy;
 
     using OBeautifulCode.AutoFakeItEasy;
     using OBeautifulCode.CodeAnalysis.Recipes;
-
+    using OBeautifulCode.CodeGen.ModelObject.Recipes;
+    using OBeautifulCode.Math.Recipes;
     using Xunit;
+
+    using static System.FormattableString;
 
     [SuppressMessage("Microsoft.Maintainability", "CA1505:AvoidUnmaintainableCode", Justification = ObcSuppressBecause.CA1505_AvoidUnmaintainableCode_DisagreeWithAssessment)]
     public static partial class StreamRecordMetadataTest
@@ -23,6 +27,120 @@ namespace Naos.Database.Domain.Test
         [SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Justification = ObcSuppressBecause.CA1810_InitializeReferenceTypeStaticFieldsInline_FieldsDeclaredInCodeGeneratedPartialTestClass)]
         static StreamRecordMetadataTest()
         {
+            ConstructorArgumentValidationTestScenarios
+               .RemoveAllScenarios()
+               .AddScenario(
+                    () =>
+                        new ConstructorArgumentValidationTestScenario<StreamRecordMetadata>
+                        {
+                            Name = "constructor should throw ArgumentNullException when parameter 'serializerRepresentation' is null scenario",
+                            ConstructionFunc = () =>
+                                               {
+                                                   var referenceObject = A.Dummy<StreamRecordMetadata>();
+
+                                                   var result = new StreamRecordMetadata(
+                                                       referenceObject.StringSerializedId,
+                                                       null,
+                                                       referenceObject.TypeRepresentationOfId,
+                                                       referenceObject.TypeRepresentationOfObject,
+                                                       referenceObject.Tags,
+                                                       referenceObject.TimestampUtc);
+
+                                                   return result;
+                                               },
+                            ExpectedExceptionType = typeof(ArgumentNullException),
+                            ExpectedExceptionMessageContains = new[]
+                                                               {
+                                                                   "serializerRepresentation",
+                                                               },
+                        })
+               .AddScenario(
+                    () =>
+                        new ConstructorArgumentValidationTestScenario<StreamRecordMetadata>
+                        {
+                            Name = "constructor should throw ArgumentNullException when parameter 'typeRepresentationOfId' is null scenario",
+                            ConstructionFunc = () =>
+                                               {
+                                                   var referenceObject = A.Dummy<StreamRecordMetadata>();
+
+                                                   var result = new StreamRecordMetadata(
+                                                       referenceObject.StringSerializedId,
+                                                       referenceObject.SerializerRepresentation,
+                                                       null,
+                                                       referenceObject.TypeRepresentationOfObject,
+                                                       referenceObject.Tags,
+                                                       referenceObject.TimestampUtc);
+
+                                                   return result;
+                                               },
+                            ExpectedExceptionType = typeof(ArgumentNullException),
+                            ExpectedExceptionMessageContains = new[]
+                                                               {
+                                                                   "typeRepresentationOfId",
+                                                               },
+                        })
+               .AddScenario(
+                    () =>
+                        new ConstructorArgumentValidationTestScenario<StreamRecordMetadata>
+                        {
+                            Name = "constructor should throw ArgumentNullException when parameter 'typeRepresentationOfObject' is null scenario",
+                            ConstructionFunc = () =>
+                                               {
+                                                   var referenceObject = A.Dummy<StreamRecordMetadata>();
+
+                                                   var result = new StreamRecordMetadata(
+                                                       referenceObject.StringSerializedId,
+                                                       referenceObject.SerializerRepresentation,
+                                                       referenceObject.TypeRepresentationOfId,
+                                                       null,
+                                                       referenceObject.Tags,
+                                                       referenceObject.TimestampUtc);
+
+                                                   return result;
+                                               },
+                            ExpectedExceptionType = typeof(ArgumentNullException),
+                            ExpectedExceptionMessageContains = new[]
+                                                               {
+                                                                   "typeRepresentationOfObject",
+                                                               },
+                        })
+               .AddScenario(
+                    () =>
+                        new ConstructorArgumentValidationTestScenario<StreamRecordMetadata>
+                        {
+                            Name =
+                                "constructor should throw ArgumentException when parameter 'timestamp' is not of kind UTC scenario",
+                            ConstructionFunc = () =>
+                                               {
+                                                   var referenceObject = A.Dummy<StreamRecordMetadata>();
+
+                                                   var referenceObjectTimestampUtc = referenceObject.TimestampUtc;
+                                                   var nonUtcTimestamp = new DateTime(
+                                                       referenceObjectTimestampUtc.Year,
+                                                       referenceObjectTimestampUtc.Month,
+                                                       referenceObjectTimestampUtc.Day,
+                                                       referenceObjectTimestampUtc.Hour,
+                                                       referenceObjectTimestampUtc.Minute,
+                                                       referenceObjectTimestampUtc.Second,
+                                                       referenceObjectTimestampUtc.Millisecond,
+                                                       DateTimeKind.Unspecified);
+
+                                                   var result = new StreamRecordMetadata(
+                                                       referenceObject.StringSerializedId,
+                                                       referenceObject.SerializerRepresentation,
+                                                       referenceObject.TypeRepresentationOfId,
+                                                       referenceObject.TypeRepresentationOfObject,
+                                                       referenceObject.Tags,
+                                                       nonUtcTimestamp);
+
+                                                   return result;
+                                               },
+                            ExpectedExceptionType = typeof(ArgumentException),
+                            ExpectedExceptionMessageContains = new[]
+                                                               {
+                                                                   "The timestamp must be in UTC format",
+                                                               },
+                        });
         }
     }
 }

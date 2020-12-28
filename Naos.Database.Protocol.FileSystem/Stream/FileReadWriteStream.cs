cfa34a21bf45a29f7f7b279bc9ec7f01fdf004c0
Lawson Caudill <lawson@getthinktank.com>
2020-12-28 20:00:33 +0000

    CA suppression.

--- Naos.Database.Protocol.FileSystem/Stream/FileReadWriteStream.cs
+++ Naos.Database.Protocol.FileSystem/Stream/FileReadWriteStream.cs
@@ -657,6 +657,7 @@ namespace Naos.Database.Protocol.FileSystem
         }
 
         /// <inheritdoc />
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity", Justification = NaosSuppressBecause.CA1502_AvoidExcessiveComplexity_DisagreeWithAssessment)]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling", Justification = NaosSuppressBecause.CA1506_AvoidExcessiveClassCoupling_DisagreeWithAssessment)]
         public override StreamRecord Execute(
             TryHandleRecordOp operation)

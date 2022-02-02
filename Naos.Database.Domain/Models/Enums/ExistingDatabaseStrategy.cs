a66cf55efad894ecdf257181cb1b607b8ba34cd6
Lawson Caudill <lawson@getthinktank.com>
2022-02-02 13:59:37 -0500

    Added ExistingDatabaseStrategy enum.

---
+++ Naos.Database.Domain/Models/Enums/ExistingDatabaseStrategy.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExistingDatabaseStrategy.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.Domain
+{
+    /// <summary>
+    /// Strategy to use when trying to create a database that already exists.
+    /// </summary>
+    public enum ExistingDatabaseStrategy
+    {
+        /// <summary>
+        /// Unknown (default).
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Skip the creation.
+        /// </summary>
+        Skip,
+
+        /// <summary>
+        /// Re-create the database.
+        /// </summary>
+        Overwrite,
+
+        /// <summary>
+        /// Throw exception.
+        /// </summary>
+        Throw,
+    }
+}

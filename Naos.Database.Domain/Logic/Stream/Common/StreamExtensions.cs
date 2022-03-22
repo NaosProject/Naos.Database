57f908144a26e95873179dc9fb1d251d3051f17c
Lawson Caudill <lawson@getthinktank.com>
2022-03-22 16:01:36 -0400

    Added GetStringSerializedIdentifier extension on streams to create a StringSerializedIdentifier from a TId.

---
+++ Naos.Database.Domain/Logic/Stream/Common/StreamExtensions.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StreamExtensions.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Representation.System;
+
+    /// <summary>
+    /// Extension methods on <see cref="IStream"/>.
+    /// </summary>
+    public static partial class StreamExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="StringSerializedIdentifier"/> from the provided <paramref name="id"/>.
+        /// </summary>
+        /// <typeparam name="TId">The type of the identifier to convert.</typeparam>
+        /// <param name="stream">The stream.</param>
+        /// <param name="id">The identifier to convert.</param>
+        /// <returns>The <see cref="StringSerializedIdentifier"/> representation of the <paramref name="id"/>.</returns>
+        public static StringSerializedIdentifier GetStringSerializedIdentifier<TId>(
+            this IStream stream,
+            TId id)
+        {
+            stream.MustForArg(nameof(stream)).NotBeNull();
+
+            var identifierSerializer = stream.SerializerFactory.BuildSerializer(stream.DefaultSerializerRepresentation);
+            var serializedIdentifier = identifierSerializer.SerializeToString(id);
+            var result = new StringSerializedIdentifier(serializedIdentifier, (id?.GetType() ?? typeof(TId)).ToRepresentation());
+
+            return result;
+        }
+    }
+}

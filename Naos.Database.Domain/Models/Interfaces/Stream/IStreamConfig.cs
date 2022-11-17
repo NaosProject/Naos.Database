44c53d104af7dc58ebad930e961c235b9fd644c0
Lawson Caudill <lawson@getthinktank.com>
2022-11-17 11:50:41 -0500

    Added IStreamConfig interface for holding necessary constiuents for constructing a StandardStream implementation.

---
+++ Naos.Database.Domain/Models/Interfaces/Stream/IStreamConfig.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IStreamConfig.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.Domain
+{
+    using System.Collections.Generic;
+    using OBeautifulCode.Serialization;
+
+    /// <summary>
+    /// Interface to expose necessary general items to create an implementation of <see cref="IStandardStream"/>.
+    /// </summary>
+    public interface IStreamConfig
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the access the stream has.
+        /// </summary>
+        StreamAccessKinds AccessKinds { get; }
+
+        /// <summary>
+        /// Gets the default <see cref="SerializerRepresentation"/> (used for identifier serialization).
+        /// </summary>
+        SerializerRepresentation DefaultSerializerRepresentation { get; }
+
+        /// <summary>
+        /// Gets the default <see cref="SerializationFormat"/>.
+        /// </summary>
+        SerializationFormat DefaultSerializationFormat { get; }
+
+        /// <summary>
+        /// Gets all <see cref="IResourceLocator"/>'s.
+        /// </summary>
+        IReadOnlyCollection<IResourceLocator> AllLocators { get; }
+    }
+}

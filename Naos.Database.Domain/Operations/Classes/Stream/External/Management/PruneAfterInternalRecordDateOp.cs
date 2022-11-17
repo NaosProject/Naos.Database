6907e035ffc5ba361b7241b119fe80da6ef1565e
Lawson Caudill <lawson@getthinktank.com>
2022-11-17 11:57:27 -0500

    Adding PruneAfterInternalRecordDateOp - not currently supported.

---
+++ Naos.Database.Domain/Operations/Classes/Stream/External/Management/PruneAfterInternalRecordDateOp.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PruneAfterInternalRecordDateOp.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Database.Domain
+{
+    using System;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Deletes all records in a <see cref="IManagementOnlyStream"/> whose internal record date is less than the specified threshold.
+    /// </summary>
+    public partial class PruneAfterInternalRecordDateOp : VoidOperationBase, IPruneOp
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PruneAfterInternalRecordDateOp"/> class.
+        /// </summary>
+        /// <param name="internalRecordDate">The internal record date; all records older will be pruned.</param>
+        /// <param name="details">Details related to the operation.</param>
+        public PruneAfterInternalRecordDateOp(
+            DateTime internalRecordDate,
+            string details)
+        {
+            internalRecordDate.Kind.MustForArg(Invariant($"{nameof(internalRecordDate)}.{nameof(internalRecordDate.Kind)}")).BeEqualTo(DateTimeKind.Utc, "Timestamp must be UTC.");
+            details.MustForArg(nameof(details)).NotBeNullNorWhiteSpace();
+
+            this.InternalRecordDate = internalRecordDate;
+            this.Details = details;
+        }
+
+        /// <summary>
+        /// Gets the internal record date to use, all records older will be pruned.
+        /// </summary>
+        public DateTime InternalRecordDate { get; private set; }
+
+        /// <inheritdoc />
+        public string Details { get; private set; }
+    }
+}
